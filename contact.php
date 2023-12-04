13f61ee87be0261eafbc9919d4eab49c986d0f0e
solange <solangeleitesg@gmail.com>
2023-12-04 11:12:08 -0300

    cors error

--- contact.php
+++ contact.php
@@ -1,5 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: https://solange-leites-galvan.vercel.app');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 
 $conexion = mysqli_connect('localhost', 'id21619166_solange', 'hummusLov3r!', 'id21619166_backendsolange') or exit('No se pudo conectar con la base de datos');
 mysqli_query($conexion, "INSERT INTO db_msgportfolio VALUES(DEFAULT, '$_POST[name]', '$_POST[email]', '$_POST[msg]')") or die("Error al insertar los datos en la base de datos");
