4e5d531f1934b3250ef0a1e983dc2cf8404b01f3
solange <solangeleitesg@gmail.com>
2023-12-04 10:56:27 -0300

    solucionando error de cors

--- contact.php
+++ contact.php
@@ -1,29 +1,25 @@
 <?php
 header('Access-Control-Allow-Origin: https://solange-leites-galvan.vercel.app');
 
-// $conexion = mysqli_connect('localhost', 'root', '', 'db_portfolio');
-// if(mysqli_connect_error()){
-//     echo "Error al conectar con la base de datos";
-//     exit();
-// } else {
-//     $name = $_POST['name'];
-//     $email = $_POST['email'];
-//     $msg = $_POST['msg'];
-
-//     $sql($conexion, "INSERT INTO db_msgportfolio VALUES(DEFAULT, '$name', '$email', '$msg')");
-//     $res = mysqli_query($conexion, $sql);
-
-//     if($res){
-//         echo "Datos insertados correctamente";
-//     } else {
-//         echo "Error al insertar los datos en la base de datos";
-//     }
-//     $conexion->close();
-// }
-
-
 $conexion = mysqli_connect('localhost', 'id21619166_solange', 'hummusLov3r!', 'id21619166_backendsolange') or exit('No se pudo conectar con la base de datos');
-mysqli_query($conexion, "INSERT INTO db_msgportfolio VALUES(DEFAULT, '$_POST[name]', '$_POST[email]', '$_POST[msg]')") or die("Error al insertar los datos en la base de datos");
 
-echo "Su mensaje ha sido enviado con éxito!" ;
- mysqli_close($conexion);
+// Obtener datos del formulario
+$name = mysqli_real_escape_string($conexion, $_POST['name']);
+$email = mysqli_real_escape_string($conexion, $_POST['email']);
+$msg = mysqli_real_escape_string($conexion, $_POST['msg']);
+
+// Consulta preparada para evitar inyección SQL
+$query = "INSERT INTO db_msgportfolio (nombre, email, mensaje) VALUES (?, ?, ?)";
+$stmt = mysqli_prepare($conexion, $query);
+mysqli_stmt_bind_param($stmt, "sss", $name, $email, $msg);
+mysqli_stmt_execute($stmt);
+
+if (mysqli_stmt_affected_rows($stmt) > 0) {
+    echo "Su mensaje ha sido enviado con éxito!";
+} else {
+    echo "Error al enviar el mensaje. Por favor, inténtelo nuevamente.";
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($conexion);
+?>
